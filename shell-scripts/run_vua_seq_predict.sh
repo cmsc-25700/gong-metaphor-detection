0a815b5305266d4fb61aac3c2b32cf1c8b7fdd4e
Cole Frank <cole.v.frank@gmail.com>
2022-03-12 16:57:00 -0600

    new shell script for vua prediction

---
+++ shell-scripts/run_vua_seq_predict.sh
@@ -0,0 +1,18 @@
+python3 run_metaphor_detection.py \
+--data_dir data/VUA \
+--model_type roberta \
+--model_name_or_path roberta-large \
+--output_dir output/VUA/modeltest/ \
+--dataset VUA \
+--max_seq_length 256 \
+--do_predict \
+#--do_train \
+#--evaluate_during_training \
+--do_lower_case \
+#--per_gpu_train_batch_size 6 \
+--per_gpu_eval_batch_size 18 \
+#--learning_rate 2e-5 \
+#--num_train_epochs 5.0 \
+#--warmup_steps 500 \
+--use_pos # \
+#--overwrite_output_dir

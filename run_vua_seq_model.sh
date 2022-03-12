26f131a0e586a9b6c694f8a5708ec4b0d36da48d
Christine Ibaraki <christine.ibaraki@gmail.com>
2022-03-12 08:46:36 -0600

    change to test dir

--- run_vua_seq_model.sh
+++ run_vua_seq_model.sh
@@ -2,7 +2,7 @@ python3 run_metaphor_detection.py \
 --data_dir data/VUA \
 --model_type roberta \
 --model_name_or_path roberta-large \
---output_dir output/VUA/model/ \
+--output_dir output/VUA/modeltest/ \
 --dataset VUA \
 --max_seq_length 256 \
 --do_train \

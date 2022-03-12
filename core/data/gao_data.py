30c00eb4ab6bfeef989a6a45b5735f2411754cf6
Christine Ibaraki <christine.ibaraki@gmail.com>
2022-03-11 20:39:47 -0600

    add more data

---
+++ core/data/gao_data.py
@@ -0,0 +1,221 @@
+"""
+Class for organizing gao et all datasets
+"""
+
+import os
+import csv
+import pandas as pd
+
+MOH = "MOH"
+MOH_X = "MOH-X"
+TROFI = "TroFi"
+TROFI_X = "TroFi-X"
+VUA = "VUA"
+VUA_SEQ = "VUA_seq"
+
+##### MOH-X and TroFi datasets
+# 10 fold cross-validation on the
+
+##### VUA dataset
+# use the original training and test split
+#  set aside 10% of the training set as a development set.
+
+
+class ExperimentData:
+    """
+    Attributes:
+        there is an attribute for every file in the gao et all repo
+        every attribute is initialized to None
+        call public read functions to read in file by dataset
+
+        e.g.read_moh_data reads all MOH datasets to respective attributes
+    """
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+
+        # MOH
+        self.moh_formatted = None
+        self.moh_formatted_train = None
+        self.moh_formatted_test = None
+        self.moh_formatted_val = None
+
+        # MOH X
+        self.moh_x_formatted_svo = None
+        self.moh_x_formatted_svo_cleaned = None
+
+        # TROFI
+        self.trofi_formatted_all = None
+
+        # TROFI-X
+        self.trofi_x_formatted_svo = None
+
+        # VUA
+        self.vua_formatted = None
+        self.vua_formatted_train = None
+        self.vua_formatted_train_augmented = None
+        self.vua_formatted_train_noVAL = None
+        self.vua_formatted_test = None
+        self.vua_formatted_val = None
+
+        # VUA sequence
+        self.vua_seq_formatted_train = None
+        self.vua_seq_formatted_test = None
+        self.vua_seq_formatted_val = None
+
+    def read_all_data(self, to_pandas = False):
+        """
+        Read all datasets
+        :param to_pandas: if true, data should be DataFrame, else list
+        :return: NA
+        """
+
+        self.read_moh_data(to_pandas)
+        self.read_moh_x_data(to_pandas)
+        self.read_trofi_data(to_pandas)
+        self.read_trofi_x_data(to_pandas)
+        self.read_vua_data(to_pandas)
+        self.read_vua_seq_data(to_pandas)
+
+    def read_moh_data(self, to_pandas=False):
+        """
+        Read data into public instance variables
+
+        :param to_pandas: if true, data should be DataFrame, else list
+        :return: NA
+        """
+        data_var = MOH
+
+        self.moh_formatted, nrow = self.read_experiment_file(
+            data_var, "{}_formatted.csv".format(data_var), to_pandas)
+        print(f"{data_var} formatted nrow: {nrow}")
+
+        self.moh_formatted_train, nrow = self.read_experiment_file(
+            data_var, "{}_formatted_train.csv".format(data_var), to_pandas)
+        print(f"{data_var} train nrow: {nrow}")
+
+        self.moh_formatted_test, nrow = self.read_experiment_file(
+            data_var, "{}_formatted_test.csv".format(data_var), to_pandas)
+        print(f"{data_var} test nrow: {nrow}")
+
+        self.moh_formatted_val, nrow = self.read_experiment_file(
+            data_var, "{}_formatted_val.csv".format(data_var), to_pandas)
+        print(f"{data_var} val nrow: {nrow}")
+
+    def read_moh_x_data(self, to_pandas=False):
+        """
+        Read data into public instance variables
+
+        :param to_pandas: if true, data should be DataFrame, else list
+        :return: NA
+        """
+        data_var = MOH_X
+        self.moh_x_formatted_svo, nrow = self.read_experiment_file(
+            data_var, "{}_formatted_svo.csv".format(data_var), to_pandas)
+        print(f"{data_var} formatted svo nrow: {nrow}")
+
+        self.moh_x_formatted_svo_cleaned, nrow = self.read_experiment_file(
+            data_var, "{}_formatted_svo_cleaned.csv".format(data_var), to_pandas)
+        print(f"{data_var} formatted svo cleaned nrow: {nrow}")
+
+    def read_trofi_data(self, to_pandas=False):
+        """
+        Read data into public instance variables
+
+        :param to_pandas: if true, data should be DataFrame, else list
+        :return: NA
+        """
+        data_var = TROFI
+        self.trofi_formatted_all, nrow = self.read_experiment_file(
+            data_var, "{}_formatted_all3737.csv".format(data_var), to_pandas)
+        print(f"{data_var} formatted_all3737 nrow: {nrow}")
+
+    def read_trofi_x_data(self, to_pandas=False):
+        """
+        Read data into public instance variables
+
+        :param to_pandas: if true, data should be DataFrame, else list
+        :return: NA
+        """
+        data_var = TROFI_X
+        self.trofi_x_formatted_svo, nrow = self.read_experiment_file(
+            data_var, "{}_formatted_svo.csv".format(data_var), to_pandas)
+        print(f"{data_var} formatted svo nrow: {nrow}")
+
+    def read_vua_data(self, to_pandas=False):
+        """
+        Read data into public instance variables
+
+        :param to_pandas: if true, data should be DataFrame, else list
+        :return: NA
+        """
+        data_var = VUA
+        encoding = 'latin-1'
+
+        self.vua_formatted, nrow = self.read_experiment_file(
+            data_var, "{}_formatted.csv".format(data_var), to_pandas, encoding)
+        print(f"{data_var} formatted nrow: {nrow}")
+
+        self.vua_formatted_train, nrow = self.read_experiment_file(
+            data_var, "{}_formatted_train.csv".format(data_var), to_pandas, encoding)
+        print(f"{data_var} train nrow: {nrow}")
+
+        self.vua_formatted_train_augmented, nrow = self.read_experiment_file(
+            data_var, "{}_formatted_train_augmented.csv".format(data_var), to_pandas, encoding)
+        print(f"{data_var} train augmented nrow: {nrow}")
+
+        self.vua_formatted_train_noVAL, nrow = self.read_experiment_file(
+            data_var, "{}_formatted_train_noVAL.csv".format(data_var), to_pandas, encoding)
+        print(f"{data_var} train no val nrow: {nrow}")
+
+        self.vua_formatted_test, nrow = self.read_experiment_file(
+            data_var, "{}_formatted_test.csv".format(data_var), to_pandas, encoding)
+        print(f"{data_var} test nrow: {nrow}")
+
+        self.vua_formatted_val, nrow = self.read_experiment_file(
+            data_var, "{}_formatted_val.csv".format(data_var), to_pandas, encoding)
+        print(f"{data_var} val nrow: {nrow}")
+
+    def read_vua_seq_data(self, to_pandas=False):
+        """
+        Read data into public instance variables
+
+        :param to_pandas: if true, data should be DataFrame, else list
+        :return: NA
+        """
+        folder = "VUAsequence"
+        data_var = VUA_SEQ
+        encoding = 'latin-1'
+
+        self.vua_seq_formatted_train, nrow = self.read_experiment_file(
+            folder, "{}_formatted_train.csv".format(data_var), to_pandas, encoding)
+        print(f"{data_var} train nrow: {nrow}")
+
+        self.vua_seq_formatted_test, nrow = self.read_experiment_file(
+            folder, "{}_formatted_test.csv".format(data_var), to_pandas, encoding)
+        print(f"{data_var} test nrow: {nrow}")
+
+        self.vua_seq_formatted_val, nrow = self.read_experiment_file(
+            folder, "{}_formatted_val.csv".format(data_var), to_pandas, encoding)
+        print(f"{data_var} val nrow: {nrow}")
+
+    def read_experiment_file(self, folder, filename, pandas, encoding=None):
+        """
+        Static method to read file
+        Input: filename
+        Output: if pandas: pd DataFrame, else: list of strings
+        """
+        filepath = os.path.join(self.data_dir, folder, filename)
+        enc = 'utf-8' if not encoding else encoding
+
+        if pandas:
+            data = pd.read_csv(filepath, encoding=enc)
+            return data, len(data)
+
+        data = []
+        with open(filepath, encoding=enc) as f:
+            lines = csv.reader(f)
+            next(lines)
+            for line in lines:
+                data.append(line)
+        f.close()
+        return data, len(data)
--- data_utils.py
+++ data_utils.py
@@ -9,50 +9,26 @@ import numpy as np
 import pickle
 
 
-
 class InputExample(object):
     """
     A sentence example for token classification
     """
-    def __init__(self, example_id, words, pos_list, biasdown_vectors=None,
-                 biasup_vectors=None, biasupdown_vectors=None,
-                 corp_vectors=None, topic_vectors=None, verbnet_vectors=None,
-                 wordnet_vectors=None, labels=None):
+    def __init__(self, example_id, words, pos_list, labels=None):
         self.example_id = example_id
         self.words = words
         self.pos_list = pos_list
-        #self.content_mask_list = content_mask_list
-        self.biasdown_vectors = biasdown_vectors
-        self.biasup_vectors = biasup_vectors
-        self.biasupdown_vectors = biasupdown_vectors
-        self.corp_vectors = corp_vectors
-        self.topic_vectors = topic_vectors
-        self.verbnet_vectors = verbnet_vectors
-        self.wordnet_vectors = wordnet_vectors
         self.labels = labels
-
 
 
 class InputFeatures(object):
     """
     Features for an example
     """
-    def __init__(self, input_ids, input_mask, segment_ids, pos_ids,
-                 biasdown_vectors=None, biasup_vectors=None,
-                 biasupdown_vectors=None, corp_vectors=None,
-                 topic_vectors=None, verbnet_vectors=None,
-                 wordnet_vectors=None, label_ids=None):
+    def __init__(self, input_ids, input_mask, segment_ids, pos_ids, label_ids=None):
         self.input_ids = input_ids
         self.input_mask = input_mask
         self.segment_ids = segment_ids
         self.pos_ids = pos_ids
-        self.biasdown_vectors = biasdown_vectors
-        self.biasup_vectors = biasup_vectors
-        self.biasupdown_vectors = biasupdown_vectors
-        self.corp_vectors = corp_vectors
-        self.topic_vectors = topic_vectors
-        self.verbnet_vectors = verbnet_vectors
-        self.wordnet_vectors = wordnet_vectors
         self.label_ids = label_ids
 
 

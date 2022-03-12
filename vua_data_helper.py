0d92ba01fcc13aaefa7a2401ae850fe1a7fd8ded
Christine Ibaraki <christine.ibaraki@gmail.com>
2022-03-11 21:58:17 -0600

    >:(

--- vua_data_helper.py
+++ vua_data_helper.py
@@ -19,9 +19,9 @@ def read_vua_examples_from_file(data_folder, mode):
     examples = []
     prefix = data_folder + mode + "_"
     if mode == "train":
-        sent_file = open(prefix+"tokens.txt", "r")
-        pos_file = open(prefix+"pos.txt", "r")
-        label_file = open(prefix+"metaphor.txt", "r")
+        sent_file = open(os.path.join(prefix,"tokens.txt"), "r")
+        pos_file = open(os.path.join(prefix,"pos.txt"), "r")
+        label_file = open(os.path.join(prefix,"metaphor.txt"), "r")
         example_id = 0
         for (sent_line, pos_line, label_line) in \
              zip(sent_file, pos_file, label_file):
@@ -37,8 +37,8 @@ def read_vua_examples_from_file(data_folder, mode):
         label_file.close()
     # test data does not have labels
     elif mode == "test":
-        sent_file = open(prefix+"tokens.txt", "r")
-        pos_file = open(prefix+"pos.txt", "r")
+        sent_file = open(os.path.join(prefix,"tokens.txt"), "r")
+        pos_file = open(os.path.join(prefix,"pos.txt"), "r")
         example_id = 0
         for (sent_line, pos_line) in \
              zip(sent_file, pos_file):

3f6049b1b652aacff37525c2d14f14908c4a2180
Christine Ibaraki <christine.ibaraki@gmail.com>
2022-03-15 12:06:01 -0500

    add pos to examples_to_features

--- cls_data_helper.py
+++ cls_data_helper.py
@@ -72,6 +72,8 @@ def convert_cls_examples_to_features(
         pad_on_left=False,
         pad_token=0,
         pad_token_segment_id=0,
+        pos_vocab={},
+        pad_pos_id=0,
         pad_token_label_id=-100,
         pad_feature_val=0,
         sequence_a_segment_id=0,
@@ -89,13 +91,16 @@ def convert_cls_examples_to_features(
         if eid % 10000 == 0:
             print("Writing example %d of %d", eid, len(examples))
         tokens = []
+        pos_ids = []
         label_ids = []
         target_verb_ids = []
-        for (word, label, target_ind) in \
-                zip(example.words, example.labels, example.targets_verbs):
+
+        for (word, pos, label, target_ind) in \
+                zip(example.words, example.pos_list, example.labels, example.targets_verbs):
             # a word might be split into multiple tokens
             word_tokens = tokenizer.tokenize(word)
             tokens += list(word_tokens)
+            pos_ids += [pos_vocab[pos]] + [pad_pos_id] * (len(word_tokens)-1)
 
             # Use the real label id for the first token of the word,
             # and padding ids for the remaining tokens
@@ -107,27 +112,32 @@ def convert_cls_examples_to_features(
         special_tokens_count = 3 if sep_token_extra else 2
         if len(tokens) > max_seq_length - special_tokens_count:
             tokens = tokens[: (max_seq_length - special_tokens_count)]
+            pos_ids = pos_ids[: (max_seq_length - special_tokens_count)]
             label_ids = label_ids[: (max_seq_length - special_tokens_count)]
             target_verb_ids = target_verb_ids[: (max_seq_length - special_tokens_count)]
 
         tokens += [sep_token]
+        pos_ids += [pad_pos_id]
         label_ids += [pad_token_label_id]
         target_verb_ids += [pad_token_label_id]
 
         if sep_token_extra:
             # RoBERTa uses an extra separator b/w pairs of sentences
             tokens += [sep_token]
+            pos_ids += [pad_pos_id]
             label_ids += [pad_token_label_id]
             target_verb_ids += [pad_token_label_id]
         segment_ids = [sequence_a_segment_id] * len(tokens)
 
         if cls_token_at_end:
             tokens += [cls_token]
+            pos_ids += [pad_pos_id]
             label_ids += [pad_token_label_id]
             target_verb_ids += [pad_token_label_id]
             segment_ids += [cls_token_segment_id]
         else:
             tokens = [cls_token] + tokens
+            pos_ids = [pad_pos_id] + pos_ids
             label_ids = [pad_token_label_id] + label_ids
             target_verb_ids = [pad_token_label_id] + target_verb_ids
             segment_ids = [cls_token_segment_id] + segment_ids
@@ -142,18 +152,21 @@ def convert_cls_examples_to_features(
             input_ids = ([pad_token] * padding_length) + input_ids
             input_mask = ([0 if mask_padding_with_zero else 1] * padding_length) + input_mask
             segment_ids = ([pad_token_segment_id] * padding_length) + segment_ids
+            pos_ids = ([pad_pos_id] * padding_length) + pos_ids
             label_ids = ([pad_token_label_id] * padding_length) + label_ids
             target_verb_ids = ([pad_token_label_id] * padding_length) + target_verb_ids
         else:
             input_ids += [pad_token] * padding_length
             input_mask += [0 if mask_padding_with_zero else 1] * padding_length
             segment_ids += [pad_token_segment_id] * padding_length
+            pos_ids += [pad_pos_id] * padding_length
             label_ids += [pad_token_label_id] * padding_length
             target_verb_ids += [pad_token_label_id] * padding_length
 
         assert len(input_ids) == max_seq_length
         assert len(input_mask) == max_seq_length
         assert len(segment_ids) == max_seq_length
+        assert len(pos_ids) == max_seq_length
         assert len(label_ids) == max_seq_length
         assert len(target_verb_ids) == max_seq_length
 
@@ -165,13 +178,14 @@ def convert_cls_examples_to_features(
             print("input_ids: %s", " ".join([str(x) for x in input_ids]))
             print("input_mask: %s", " ".join([str(x) for x in input_mask]))
             print("segment_ids: %s", " ".join([str(x) for x in segment_ids]))
+            print("pos_ids: %s", " ".join([str(x) for x in pos_ids]))
             print("label_ids: %s", " ".join([str(x) for x in label_ids]))
             print("target_verb_ids: %s", " ".join([str(x) for x in target_verb_ids]))
 
         features.append(InputFeatures(input_ids=input_ids,
                                       input_mask=input_mask,
                                       segment_ids=segment_ids,
-                                      pos_ids=None,
+                                      pos_ids=pos_ids,
                                       label_ids=label_ids,
                                       target_verb_ids=target_verb_ids))
     print("# of examples: {}, avg sent_len: {}".format(len(sent_lens), np.mean(sent_lens)))
